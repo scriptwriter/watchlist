d97aea448daa77c9f1df850b0bc6714dbdb39be6
amit <umarye.amit@gmail.com>
2018-10-25 21:44:40 +0000

    update - add from 52w high/low

--- __init__.py
+++ __init__.py
@@ -5,18 +5,56 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+
+def extract_label_val(label):
+    if label=='dividend':
+        return soup.find("span", {"id":label}).contents[0]
+    else:
+        return round(float(soup.find("span", {"id":label}).contents[0].strip().replace(',', '')))
+
+def find_drop(current_price, year_high, year_low):
+    from_year_high = 0
+    from_year_low = 0
+    diff_from_year_high = year_high - current_price
+    diff_from_year_low = current_price - year_low
+
+    if diff_from_year_high <= 0:
+        from_year_high =  '52w HIGH'
+    else:
+        from_year_high = round(diff_from_year_high * 100 / year_high)
+
+    if diff_from_year_low <= 0:
+        from_year_low =  '52w LOW'
+    else:
+        from_year_low = round(diff_from_year_low * 100 / year_low)
+
+    return from_year_high, from_year_low
+
+
 resp = requests.get('http://www.ratestar.in/company/asianpaint/500820/Asian-Paints-Ltd-100820')
 soup = BeautifulSoup(resp.text, 'lxml')
 
-#soup.find("span", {"id":"lblMCap"})
-#soup.find("span", {"id":"lblStockPE"})
-
 fmcg = []
+market_cap=extract_label_val('lblMCap')
+current_price=extract_label_val('lblLTP') 
+year_high=extract_label_val('lblwHigh')
+year_low=extract_label_val('lblwLow')
+from_year_high, from_year_low=find_drop(current_price, year_high, year_low)
+price_to_earnings=extract_label_val('lblStockPE') 
+price_to_earnings_8=extract_label_val('lblPEAvg8')
+price_to_earnings_5=extract_label_val('lblPEAvg5')
+price_to_earnings_3=extract_label_val('lblPEAvg3')
+
 an_item = dict(scrip="Asian Paints", 
-               market_cap=soup.find("span", {"id":"lblMCap"}).contents[0], 
-               price_to_earnings=soup.find("span", {"id":"lblStockPE"}).contents[0], 
-               price_to_sales=0, 
-               peg=0)
+               market_cap=market_cap, 
+               current_price=current_price, 
+               from_year_high=from_year_high,
+               from_year_low=from_year_low,
+               price_to_earnings=price_to_earnings, 
+               price_to_earnings_8=price_to_earnings_8, 
+               price_to_earnings_5=price_to_earnings_5, 
+               price_to_earnings_3=price_to_earnings_3 
+               )
 fmcg.append(an_item)
 
 
